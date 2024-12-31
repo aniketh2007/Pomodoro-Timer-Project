69f0ee22834f70c7e3adc3bc257113aa68c5a3b5
Aniketh H <61650767+aniketh2007@users.noreply.github.com>
2024-12-31 18:51:54 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,94 +1,100 @@
 from tkinter import *
 import math
-
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 30
 SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+LONG_BREAK_MIN = 30
 reps = 0
 timer = None
+# ---------------------------- TIMER RESET ------------------------------- # 
 
-
-# ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
     window.after_cancel(timer)
     canvas.itemconfig(timer_text,text="00:00")
     title_label.config(text="Timer")
-    check_marks.config(text="")
+    check_mark.config(text="")
     global reps
     reps = 0
 
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global reps
     reps += 1
-    work_sec = WORK_MIN * 60
+    
+    work_secs= WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
-
-    # if its the 8th rep: if modular of 8 doesnt give any remainder then its long break
     if reps % 8 == 0:
         count_down(long_break_sec)
-        title_label.config(text="Break", fg=RED)
-    # if its the 2nd/4th/6th rep: if modular of 2 doesnt give any remainder then its short break
+        title_label.config(text="Long Break",fg=RED)
     elif reps % 2 == 0:
         count_down(short_break_sec)
-        title_label.config(text='Break', fg=PINK)
-    else:  # else then its normal time
-        count_down(work_sec)
-        title_label.config(text="Work", fg=GREEN)
+        title_label.config(text="Break",fg=PINK)
+    else:
+        count_down(work_secs)
+        title_label.config(text="Work",fg=GREEN)
+        
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-def count_down(count):
+#By using after method and adding counting mechanism
+def count_down(count): 
     count_min = math.floor(count / 60)
     count_sec = count % 60
-
+    # Dynamic Typing
     if count_sec < 10:
         count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
-
     if count > 0:
         global timer
         timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
-        marks = ''
-        work_sessions = math.floor(reps / 2)
+        marks = ""
+        work_sessions = math.floor(reps/2)
         for _ in range(work_sessions):
-            marks += '✓'
-            check_marks.config(text=marks)
-
-
+            marks += "✔"
+        check_mark.config(text=marks)
+            
 # ---------------------------- UI SETUP ------------------------------- #
+# Main screen
 window = Tk()
-window.title('Pomodoro')
-window.config(padx=100, pady=50, bg=YELLOW)
+window.title("Pomodoro Timer")
+window.config(pady=50,padx=100,bg=YELLOW)
 
-title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, highlightthickness=0, font=(FONT_NAME, 40))
-title_label.grid(column=1, row=0)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file='tomato.png')
+#Timer heading Label
+title_label = Label(text="Timer",fg= GREEN,font=(FONT_NAME,50,"bold"),bg=YELLOW)
+title_label.grid(row=0,column=1)
 
-canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
-canvas.grid(column=1, row=1)
+#Check Mark Label
+check_mark = Label(fg= GREEN,bg=YELLOW)
+check_mark.grid(column=1,row=3)
 
-start_button = Button(text="Start", highlightthickness=0, command=start_timer)
-start_button.grid(column=0, row=2)
+#Canvas widget and setting the image onto the screen as well as setting the text onto the image
+canvas = Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100,112,image=tomato_img)
+timer_text= canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
+canvas.grid(row=1,column=1)
 
-reset_button = Button(text="Reset", highlightthickness=0,command=reset_timer)
-reset_button.grid(column=2, row=2)
 
-check_marks = Label(fg=GREEN, bg=YELLOW)
-check_marks.grid(column=1, row=3)
+#Buttons
+#Start button
+start_button= Button(text="Start",highlightthickness=0,command=start_timer)
+start_button.grid(row=2,column=0,padx=10,pady=10)
+
+#Reset button
+reset_button = Button(text="Reset",highlightthickness=0,command=reset_timer)
+reset_button.grid(row=2,column=2,padx=10,pady=10)
+ 
+
 
 window.mainloop()
+    
